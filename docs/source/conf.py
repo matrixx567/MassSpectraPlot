326bcf430296cafb42cffacbee9d2816070cf69b
Martin <martin@mnprojects.net>
2015-08-16 12:20:30 +0200

    Remove local rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,7 +99,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
+html_theme = 'default'
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
@@ -109,7 +109,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     #html_theme_path = ['_themes']
 
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
